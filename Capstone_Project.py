8b243d9b0bcc08861180eb5d0c8abf54c9127e63
Joseph Tang <jotang1@gmail.com>
2019-05-15 18:19:17 -0400

    This is a Capstone prohject for Applied Data Science Capstone

---
+++ Capstone_Project.py
@@ -0,0 +1,22 @@
+
+# coding: utf-8
+
+# This notebook is mainloy for a capstone project in Applied Data Science Capstone
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
