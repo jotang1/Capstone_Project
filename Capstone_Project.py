7c1eb928dcb189b71d1495b6d4cedf8b5da72e2a
jotang1 <50674882+jotang1@users.noreply.github.com>
2019-05-16 01:28:41 -0400

    Delete Capstone_Project.py

--- Capstone_Project.py
+++
@@ -1,5 +0,0 @@
-# This notebook is mainloy for a capstone project in Applied Data Science Capstone
-import pandas as pd
-import numpy as np
-print("Hello Capstone Project Course!")
-https://dataplatform.cloud.ibm.com/analytics/notebooks/v2/7c8f0f75-1848-4e54-8661-004d831c0e8e/view?access_token=b110fe7481084f8206229a65ee00a3cf5467dac14f264af7921a2ab034563b4e
